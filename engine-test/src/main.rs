5200a509205fee7689cc7b4a40f9f1d67694c6bb
rain-gayming <raingayming@gmail.com>
2025-01-10 21:50:14 +0000

    file fixing

--- engine-test/src/main.rs
+++ engine-test/src/main.rs
@@ -1,15 +1,23 @@
 #![allow(
     dead_code,
     unused_variables,
+    unused_imports,
     clippy::too_many_arguments,
     clippy::unnecessary_wraps
 )]
 
-use anyhow::Result;
+use anyhow::{anyhow, Ok, Result};
+use log::*;
+use vulkanalia::loader::{LibloadingLoader, LIBRARY};
+use vulkanalia::prelude::v1_0::*;
+use vulkanalia::window as vk_window;
+use vulkanalia::Version;
 use winit::dpi::LogicalSize;
 use winit::event::{Event, WindowEvent};
 use winit::event_loop::EventLoop;
 use winit::window::{Window, WindowBuilder};
+
+const PORTABILITY_MACOS_VERSION: Version = Version::new(1, 3, 216);
 
 fn main() -> Result<()> {
     pretty_env_logger::init();
@@ -31,14 +39,18 @@ fn main() -> Result<()> {
             Event::AboutToWait => window.request_redraw(),
             Event::WindowEvent { event, .. } => match event {
                 // Render a frame if our Vulkan app is not being destroyed.
-                WindowEvent::RedrawRequested if !elwt.exiting() => unsafe { app.render(&window) }.unwrap(),
+                WindowEvent::RedrawRequested if !elwt.exiting() => {
+                    unsafe { app.render(&window) }.unwrap()
+                }
                 // Destroy our Vulkan app.
                 WindowEvent::CloseRequested => {
                     elwt.exit();
-                    unsafe { app.destroy(); }
+                    unsafe {
+                        app.destroy();
+                    }
                 }
                 _ => {}
-            }
+            },
             _ => {}
         }
     })?;
@@ -46,14 +58,56 @@ fn main() -> Result<()> {
     Ok(())
 }
 
+unsafe fn create_instance(window: &Window, entry: &Entry) -> Result<Instance> {
+    // info about the app, name, version, engine name, api ect
+    let application_info = vk::ApplicationInfo::builder()
+        .application_name(b"Vulkan Tutorial\0")
+        .application_version(vk::make_version(1, 0, 0))
+        .engine_name(b"No Engine\0")
+        .engine_version(vk::make_version(1, 0, 0))
+        .api_version(vk::make_version(1, 0, 0));
+
+    let mut extensions = vk_window::get_required_instance_extensions(window)
+        .iter()
+        .map(|e| e.as_ptr())
+        .collect::<Vec<_>>();
+
+    // if youre on a mac do some mac needed shit otherwise do the standard stuff
+    let flags = if cfg!(target_os = "macos") && entry.version()? >= PORTABILITY_MACOS_VERSION {
+        info!("Enabling extensions for macOS portability.");
+        extensions.push(
+            vk::KHR_GET_PHYSICAL_DEVICE_PROPERTIES2_EXTENSION
+                .name
+                .as_ptr(),
+        );
+        extensions.push(vk::KHR_PORTABILITY_ENUMERATION_EXTENSION.name.as_ptr());
+        vk::InstanceCreateFlags::ENUMERATE_PORTABILITY_KHR
+    } else {
+        vk::InstanceCreateFlags::empty()
+    };
+
+    let info = vk::InstanceCreateInfo::builder()
+        .application_info(&application_info)
+        .enabled_extension_names(&extensions)
+        .flags(flags);
+
+    Ok(entry.create_instance(&info, None)?)
+}
+
 /// Our Vulkan app.
 #[derive(Clone, Debug)]
-struct App {}
+struct App {
+    entry: Entry,
+    instance: Instance,
+}
 
 impl App {
     /// Creates our Vulkan app.
     unsafe fn create(window: &Window) -> Result<Self> {
-        Ok(Self {})
+        let loader = LibloadingLoader::new(LIBRARY)?;
+        let entry = Entry::new(loader).map_err(|b| anyhow!("{}", b))?;
+        let instance = create_instance(window, &entry)?;
+        Ok(Self { entry, instance })
     }
 
     /// Renders a frame for our Vulkan app.
@@ -62,7 +116,9 @@ impl App {
     }
 
     /// Destroys our Vulkan app.
-    unsafe fn destroy(&mut self) {}
+    unsafe fn destroy(&mut self) {
+        self.instance.destroy_instance(None);
+    }
 }
 
 /// The Vulkan handles and associated properties used by our Vulkan app.
