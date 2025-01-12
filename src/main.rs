8a9ccebaed86e39e30621052851ef85bb8650af5
rain-gayming <raingayming@gmail.com>
2025-01-12 21:05:39 +0000

    made the screen bigger, added versin text and changed the background colour

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,15 @@ use raylib::prelude::*;
 fn main() {
     //info on the window
     let (mut rl, thread) = raylib::init()
-        .size(640, 480)
+        .size(1280, 720)
         .title("voxel engine fr this time")
         .build();
 
     while !rl.window_should_close() {
         let mut d = rl.begin_drawing(&thread);
 
-        //renders a hello world text
-        d.clear_background(Color::WHITE);
-        d.draw_text("Hello, world!", 12, 12, 20, Color::BLACK);
+        //renders the version text
+        d.clear_background(Color::GRAY);
+        d.draw_text("Voxel Engine v0.0.1a", 12, 12, 12, Color::BLACK);
     }
 }
